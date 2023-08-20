7168319a676596f4341825f73c35a4406fbb530f
Nikita Kolychev <97474520+TheSpaceMan915@users.noreply.github.com>
2023-08-20 07:10:04 +0000

    Update User

--- Lab5/src/main/java/classes/task2/User.java
+++ Lab5/src/main/java/classes/task2/User.java
@@ -1,7 +1,6 @@
 package classes.task2;
 
-public record User(String name,
-                   String m_login, 
+public record User(String m_login, 
                    long m_hash_password, 
                    String m_email, 
                    String m_phone_number) {
