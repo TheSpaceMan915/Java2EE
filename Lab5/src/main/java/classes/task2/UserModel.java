0b2821341940fdb16770db489de879e6e4a5b3ca
Nikita Kolychev <97474520+TheSpaceMan915@users.noreply.github.com>
2023-08-28 07:32:19 +0000

    Update UserModel

--- Lab5/src/main/java/classes/task2/UserModel.java
+++ Lab5/src/main/java/classes/task2/UserModel.java
@@ -19,8 +19,6 @@ public class UserModel  implements Serializable
 
     private String m_user_phone_number;
 
-    private String m_user_code;
-
     private final List<User> m_list_users = new ArrayList<>();
     {
         //filling the list with users
