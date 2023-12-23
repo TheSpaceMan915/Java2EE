208fd89c23491699367b0b34cd8418bec322b76c
Nikita Kolychev <97474520+TheSpaceMan915@users.noreply.github.com>
2023-12-23 09:40:01 +0300

    Update ValCurs

--- Lab2/src/main/java/task4_classes/currency_one_day/ValCurs.java
+++ Lab2/src/main/java/task4_classes/currency_one_day/ValCurs.java
@@ -13,8 +13,6 @@ public class ValCurs implements Serializable
     @XmlElement(name = "Valute")
     private final List<Valute> m_list_valutes = new ArrayList<>();
 
-    private int maxValuteN;
-
     public ValCurs() {}
 
     public List<Valute> getListValutes() { return m_list_valutes; }
