7bef790e56e24bd69900c2d6d2d78a9953d11cd2
Nikita Kolychev <97474520+TheSpaceMan915@users.noreply.github.com>
2023-12-26 09:44:55 +0300

    Update ValCursPeriod

--- Lab2/src/main/java/task4_classes/currency_period/ValCursPeriod.java
+++ Lab2/src/main/java/task4_classes/currency_period/ValCursPeriod.java
@@ -23,6 +23,8 @@ public class ValCursPeriod
     @XmlAttribute(name = "name")
     private String m_name;
 
+    private double price;
+
     @XmlElement(name = "Record")
     private final List<ValutePeriod> m_list_valutes = new ArrayList<>();
 
